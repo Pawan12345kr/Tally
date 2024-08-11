338f3895616d7f09eb76a3b4ec7858f79f484181
Pawan12345kr <pawan8271kr@gmail.com>
2024-08-11 11:50:42 +0530

    Your commit message

---
+++ README.md
@@ -0,0 +1,30 @@
+## Getting Started
+
+1. Make sure you have [python](https://www.python.org) and [docker](https://docs.docker.com/get-docker/) installed on your machine.
+
+1. Setup and start virtual environment. If you don't have `python3`, replace with `python`.
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+2. Install dependencies and migrate
+
+```bash
+pip install -r requirements.txt
+python manage.py migrate
+```
+
+3. Run server and open *localhost:8000* in a browser
+
+```bash
+python manage.py runserver
+```
+
+```bash
+python manage.py test
+```
+
+## Reference
+- [AlgoTutor](https://github.com/adn222/algotutor)
---
+++ static/code/code.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+// Function to sort an array of 0s, 1s, and 2s
+void solve(int arr[], int size) {
+    int low = 0, mid = 0, high = size - 1;
+
+    while (mid <= high) {
+        switch (arr[mid]) {
+            case 0:
+                swap(arr[low++], arr[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(arr[mid], arr[high--]);
+                break;
+        }
+    }
+}
+
+
+int main(){
+    
+    int size;
+    cin>>size;
+    int * arr = new int[size];
+
+    for(int i = 0;i<size;i++){
+        cin>>arr[i];
+    }
+
+    solve(arr,size);
+
+    for(int i = 0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
